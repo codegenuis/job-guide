934d828139c656cdb9d917ca4998cbd338aa6506
onyeka nworie <onyeka@emplug.com>
2018-05-02 18:12:32 +0100

    updates app js

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Register from './components/register';
 
 export default class App extends React.Component {
   render() {
